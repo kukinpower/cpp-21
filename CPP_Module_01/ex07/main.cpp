2614acf368bd64293990d9d425ee4d0fe8c3dbe4
kukinpower <thekukinpower@gmail.com>
2020-12-16 06:09:12 +0300

    add Makefile and change main parameters and body d01ex07

--- CPP_Module_01/ex07/main.cpp
+++ CPP_Module_01/ex07/main.cpp
@@ -1,6 +1,12 @@
 #include "File.hpp"
 
-int		main(void) {
+int		main(int ac, char const *av[]) {
+	if (ac != 4) {
+		std::cout << "Bad args" << std::endl;
+		return 1;
+	}
+	File	file(av[1]);
+
 
 	return 0;
 }
