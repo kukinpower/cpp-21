e9c6406248192a1e6aa9e023bc22f7350c37e3c7
kukinpower <thekukinpower@gmail.com>
2021-01-06 03:21:45 +0300

    change test main

--- CPP_Module_05/ex01/main.cpp
+++ CPP_Module_05/ex01/main.cpp
@@ -1,41 +1,6 @@
 #include "Bureaucrat.hpp"
 #include "Form.hpp"
 
-void 	testBureaucrat() {
-	try {
-		Bureaucrat	a("John", 151);
-	}
-	catch (std::exception &ex) {
-		std::cout << ex.what() << std::endl;
-	}
-	try {
-		Bureaucrat	b("Jack", 0);
-	}
-	catch (std::exception &ex) {
-		std::cout << ex.what() << std::endl;
-	}
-	Bureaucrat	a("John", 150);
-	std::cout << a;
-	try {
-		a.incrementGrade();
-		std::cout << a;
-		a.decrementGrade();
-		a.decrementGrade();
-	}
-	catch (std::exception &ex) {
-		std::cout << ex.what() << std::endl;
-	}
-	Bureaucrat	b("Jack", 1);
-	try {
-		b.incrementGrade();
-	}
-	catch (std::exception &ex) {
-		std::cout << ex.what() << std::endl;
-	}
-
-	std::cout << a << b;
-}
-
 int		main(void) {
 
 	Form	a("High", 3, 5);
