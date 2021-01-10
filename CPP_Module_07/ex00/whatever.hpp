01acca3e9190338e6e662b93d83ac5e331ee92ce
kukinpower <thekukinpower@gmail.com>
2021-01-10 19:40:55 +0300

    polish ex00

--- CPP_Module_07/ex00/whatever.hpp
+++ CPP_Module_07/ex00/whatever.hpp
@@ -3,19 +3,19 @@
 #include <iostream>
 
 template <typename T>
-void	swap(T &a, T &b) {
+void		swap(T &a, T &b) {
 	T tmp = a;
 	a = b;
 	b = tmp;
 }
 
 template <typename T>
-T		min(const T &a, const T &b) {
+const T		&min(const T &a, const T &b) {
 	return b <= a ? b : a;
 }
 
 template <typename T>
-T		max(const T &a, const T &b) {
+const T		&max(const T &a, const T &b) {
 	return b >= a ? b : a;
 }
 
